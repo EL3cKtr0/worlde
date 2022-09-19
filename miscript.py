ad2be43c88f5f7061d90d57179b62dbb71372b45
Luigi Ferdinando Iazzetta <luigiiazzetta98@gmail.com>
2022-09-19 20:53:53 +0200

    Primo commit con liste di parole

---
+++ miscript.py
@@ -0,0 +1,27 @@
+INPUT_FILE = "./input/660000_parole_italiane.txt"
+FIRST = "./list/lista_"
+THIRD = "_wordle_ita.txt"
+
+def create_files():
+    for c in range(4, 10):
+        NAME_OUTPUT = FIRST + str(c) + THIRD
+        OUTPUT = open(NAME_OUTPUT, "w")
+        OUTPUT.close()
+
+def write_on_file(c, name):
+    INPUT = open(INPUT_FILE, "r")
+    OUT = open(name, "w")
+    for word in INPUT:
+        if len(word) == c + 1:
+            OUT.write(word)
+    INPUT.close()
+    OUT.close()
+
+print("Generating files..")
+create_files()
+
+print("Writing on files..")
+for c in range(4, 10):
+    NAME_OUTPUT = FIRST + str(c) + THIRD
+    write_on_file(c, NAME_OUTPUT)
+
